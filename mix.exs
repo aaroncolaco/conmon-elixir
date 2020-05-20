c72ece783be629d61f8bb1a785b5aafffcb34874
Aaron de Miranda Colaco <aaroncolaco.work@gmail.com>
2020-05-20 16:15:44 +0530

    Update deps

--- mix.exs
+++ mix.exs
@@ -35,27 +35,21 @@ defmodule ConMon.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.4", runtime: false},
-      {:shoehorn, "~> 0.4"},
-      {:ring_logger, "~> 0.6"},
+      {:nerves, "~> 1.6.3", runtime: false},
+      {:shoehorn, "~> 0.6"},
+      {:ring_logger, "~> 0.8"},
       {:toolshed, "~> 0.2"},
       {:hackney, "~> 1.15.1"},
       {:poison, "~> 4.0"},
-      {:plug_cowboy, "~> 2.1.0"},
+      {:plug_cowboy, "~> 2.2.1"},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.6", targets: @all_targets},
-      {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
-      {:nerves_time, "~> 0.2", targets: @all_targets},
+      {:nerves_runtime, "~> 0.11.1", targets: @all_targets},
+      {:nerves_init_gadget, "~> 0.7", targets: @all_targets},
+      {:nerves_time, "~> 0.4.1", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.6", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.6", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.6", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.6", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.6", runtime: false, targets: :rpi3a},
-      {:nerves_system_bbb, "~> 2.0", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.6", runtime: false, targets: :x86_64}
+      {:nerves_system_rpi3, "~> 1.11.1", runtime: false, targets: :rpi3}
     ]
   end
 end
